b91fc750cca212f4da66c84d456e47ca24e46a10
Daryan Zycki <da.zycki@gmail.com>
2022-04-23 16:40:01 +0000

    Refactor getUserWithEmail, getUserWithId, addUser to retrieve data from our database instead of the in-memory data.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -16,18 +16,22 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => {
+    if(result.rows) { // checks if valid result (ie. user) is found in db
+      return result.rows[0]; // returns first result from array
+    } else { // if no valid result is found, returns null
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,7 +40,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    if(result.rows) { // checks if valid result (ie. user) is found in db
+      return result.rows[0]; // returns first result from array
+    } else { // if no valid result is found, returns null
+      return null;
+    }
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -46,12 +61,22 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
+  .then((result) => {
+    if(result.rows) { // checks if valid result (ie. user) is found in db
+      return result.rows[0]; // returns first result from array
+    } else { // if no valid result is found, returns null
+      return null;
+    }
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
+
 exports.addUser = addUser;
 
 /// Reservations
